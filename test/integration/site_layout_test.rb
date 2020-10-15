c3c06e5981d51e14a4e2db0d9f4820b2c6d0a22d
andrey678 <drew6496@yandex.ru>
2020-10-15 20:25:32 +0300

    Add header validation to the integration test site_layout_test (solves exercise 5.2)

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -10,5 +10,9 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]",contact_path
     assert_select "a[href=?]",signup_path
   end
-
+   test "header validation" do
+    get signup_path
+    assert_template 'users/new'
+    assert_select "h1", "Sign up"
+   end
 end
