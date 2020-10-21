8741c0ec1733b6efe056c42972dad341465b806c
andrey678 <drew6496@yandex.ru>
2020-10-21 22:19:46 +0300

    Add a test for converting an email address to lowercase (solves exercise 6.1)

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -54,6 +54,13 @@ end
   assert_not duplicate_user.valid?
   end
 
+  test "email addresses should be saved as lower-case" do
+    mixed_case_email = "Foo@ExAMPle.CoM"
+    @user.email = mixed_case_email
+    @user.save
+    assert_equal mixed_case_email.downcase, @user.reload.email
+  end
+
 test "password should be present(nonblank)" do
     @user.password = @user.password_confirmation = " " * 6
     assert_not @user.valid?
@@ -64,5 +71,7 @@ test "password should be present(nonblank)" do
     @user.password = @user.password_confirmation = "a" * 5
     assert_not @user.valid?
   end
+
+
 end
 
